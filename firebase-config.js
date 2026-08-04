2dd6155cd7e11f6f00c43f85c883adf7cf7c92e4
mhmakrostore-tech <mh.makrostore@gmail.com>
2026-08-04 14:33:30 -0400

    Add files via upload

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
- apiKey: "AIzaSyAz84bwA97vvqv7LrjvYXBI7BheRwGM_ms",
-  authDomain: "makro-tht.firebaseapp.com",
-  projectId: "makro-tht",
-  storageBucket: "makro-tht.firebasestorage.app",
-  messagingSenderId: "777366139530",
-  appId: "1:777366139530:web:2cc8ca4c2dfffb9e07f373",
-  measurementId: "G-K9SV5ZLM83"
-};
