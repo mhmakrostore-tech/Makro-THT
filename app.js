16ad2082081e9a355a2d49dbe1cacc8995aef44e
mhmakrostore-tech <mh.makrostore@gmail.com>
2026-08-04 09:12:49 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-app.js';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signInWithRedirect, getRedirectResult, onAuthStateChanged, signOut } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-auth.js';
 import { getFirestore, collection, doc, setDoc, addDoc, deleteDoc, onSnapshot, serverTimestamp, query, orderBy } from 'https://www.gstatic.com/firebasejs/12.1.0/firebase-firestore.js';
-import { firebaseConfig } from './firebase-config.js';
+import { firebaseConfig } from './firebase-config.js?v=5';
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
